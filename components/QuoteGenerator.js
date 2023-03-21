b7ae2929462113815bd523a1dd04029c8ac08bdc
Pierre CHAPELOT <pierre.chapelot@gmail.com>
2023-03-21 16:33:11 +0100

    ready to deploy

--- components/QuoteGenerator.js
+++ components/QuoteGenerator.js
@@ -19,11 +19,17 @@ const QuoteGenerator = () => {
   }, []);
 
   return (
-    <div className="flex items-center justify-center flex-col bg-[#FCFFEB] h-screen w-screen space-y-2 ">
-      <div className="text-3xl text-[#474350] font-light ">{quote}</div>
-      <div className="font-semibold">{author}</div>
-
-      <button className=" bg-[#FCFFEB] hover:bg-[#474350] text-[#474350] hover:text-[#FCFFEB] font-semibold py-2 px-4 border border-gray-400 rounded shadow" onClick={() => window.location.reload()}>New Quote</button>
+    <div className="flex items-center justify-center flex-col bg-[#FCFFEB] h-screen w-screen ">
+      <div className="space-y-2 mx-3">
+        <div className="text-3xl text-[#474350] font-light ">{quote}</div>
+        <div className="font-semibold">{author}</div>
+      </div>
+      <button
+        className="mt-10 bg-[#FCFFEB] hover:bg-[#474350] text-[#474350] hover:text-[#FCFFEB] font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+        onClick={() => window.location.reload()}
+      >
+        Get your new quote
+      </button>
       {/* <h1 className="text-3xl font-bold italic underline">Hello world!</h1> */}
     </div>
   );
