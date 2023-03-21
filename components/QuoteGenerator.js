706f8f10926f65755b5e7b35a21fb55f540ea532
Pierre CHAPELOT <pierre.chapelot@gmail.com>
2023-03-21 16:21:56 +0100

    bg color and disposition

--- components/QuoteGenerator.js
+++ components/QuoteGenerator.js
@@ -1,36 +1,32 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
-import Quote from './Quote'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+// import Quote from "./Quote";
 
 const QuoteGenerator = () => {
-    const [quote, setQuote] = useState("");
-    const [author, setAuthor] = useState("");
-  
-    useEffect(() => {
-      axios
-        .get("https://api.quotable.io/random")
-        .then((response) => {
-          setQuote(response.data.content);
-          setAuthor(response.data.author);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    }, []);
-  
-    return (
-        <div>
-      <div>{quote}</div>
-      <div>{author}</div>
-        {/* <Quote quote={quote} author={author} /> */}
-        <button onClick={() => window.location.reload()}>New Quote</button>
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
 
+  useEffect(() => {
+    axios
+      .get("https://api.quotable.io/random")
+      .then((response) => {
+        setQuote(response.data.content);
+        setAuthor(response.data.author);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-      </div>
-    );
-  };
-  
+  return (
+    <div className="flex items-center justify-center flex-col bg-[#FCFFEB] h-screen w-screen space-y-2 ">
+      <div className="text-3xl text-[#474350] font-light ">{quote}</div>
+      <div className="font-semibold">{author}</div>
 
-
+      <button className=" bg-[#FCFFEB] hover:bg-[#474350] text-[#474350] hover:text-[#FCFFEB] font-semibold py-2 px-4 border border-gray-400 rounded shadow" onClick={() => window.location.reload()}>New Quote</button>
+      {/* <h1 className="text-3xl font-bold italic underline">Hello world!</h1> */}
+    </div>
+  );
+};
 
 export default QuoteGenerator;
